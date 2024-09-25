4349171941c73b502bd65aefa95145b95d5ef7f5
samuzone <163036756+samuzone@users.noreply.github.com>
2024-09-25 07:36:15 -0500

    Add files via upload

---
+++ doug.meme/js/main.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const carousel = document.querySelector(".carousel");
+//   const contents = document.querySelectorAll(".carousel__content");
+
+//   function resetCarousel() {
+//     if (carousel.scrollLeft >= contents[0].offsetWidth) {
+//       carousel.scrollLeft = 0;
+//     }
+//   }
+
+//     carousel.addEventListener("scroll", resetCarousel);
+//   carousel.scrollLeft = 1;
+// });
